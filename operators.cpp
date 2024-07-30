4a6f7058840389b527bd3b3de66cadf527d1a69a
Amannnnnn06 <amannnnnn1405@gmail.com>
2024-07-30 19:47:34 +0530

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,24 @@
+//Name:Aman Prajapati
+//PRN:23070123017
+//Aim:Implemention of Operators
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Enter No. for a: ";
+    cin >> a;
+    cout << "Enter No. for b: ";
+    cin >> b;
+
+    c = a + b;
+    cout << "The sum is: " << c << endl;
+
+    return 0;
+}
+//output
+//Enter No. for a: 65
+//Enter No. for b: 79
+//The sum is: 144
